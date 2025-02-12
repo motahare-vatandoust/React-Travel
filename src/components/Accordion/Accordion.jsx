d748be3486bcd05b2fd2269e54499f807d487f4c
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-12 19:27:07 +0330

    Implement Accordion context for state management

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,3 +1,32 @@
+import { createContext, useContext, useState } from "react";
+
+const AccordionContext = createContext();
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext);
+  if (!ctx) {
+    throw new Error(
+      "Accordion-related components must be wrapped by <Accordion>"
+    );
+  }
+  return ctx;
+}
 export default function Accordion({ children, className }) {
-  return <ul className={className}>{children}</ul>;
+  const [openItemId, setOpenItemId] = useState();
+
+  function openItem(id) {
+    setOpenItemId(id);
+  }
+  function closeItem(id) {
+    setOpenItemId(null);
+  }
+  const contextValue = {
+    openItemId,
+    openItem,
+    closeItem,
+  };
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
 }
