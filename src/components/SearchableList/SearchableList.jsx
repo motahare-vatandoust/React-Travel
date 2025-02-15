f7fb4005de55432e21f92ed2b93e911a3c57d20a
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-15 17:59:05 +0330

    Implement debounce for SearchableList search input

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function SearchableList({ items, itemKeyFn, children }) {
+  const lastChange = useRef(null);
   const [searchTerm, setSearchTerm] = useState("");
   const searchResults = items.filter((item) =>
     JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
   );
   function handleSearch(e) {
-    setSearchTerm(e.target.value);
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null;
+      setSearchTerm(e.target.value);
+    }, 500);
   }
   return (
     <div className="searchable-list">
